f4403265a916dd24012f57b754e2c39601dee11e
Electricman <81240220+ElectricmanYT@users.noreply.github.com>
2024-07-11 21:03:02 +0700

    WhiteText

--- naara/src/app/page.tsx
+++ naara/src/app/page.tsx
@@ -1,18 +1,13 @@
-import Head from 'next/head';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className="flex w-full h-full min-h-screen flex-col items-center justify-between p-24 bg-[#0F0F0F]">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <a>
-          test
-        </a>
+        <h1 className='text-white'>
+          <Link href='/RequestPage'>map</Link>
+        </h1>
       </div>
-      <Head>
-        <title>
-          naara.
-        </title>
-      </Head>
     </main>
   );
 }
