cd6ed6873e638b255db5cc670396ce6915bfce3f
Winner R. Rasendriya <118193112+HykoU31@users.noreply.github.com>
2024-07-12 01:31:25 +0700

    map alhamdulillah bisa hore
    markers udah 3
    
    next step kasih detail

--- naara/src/app/map/page.tsx
+++ naara/src/app/map/page.tsx
@@ -5,21 +5,35 @@ import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 const Map: React.FC = () => {
   const mapContainerStyle = {
     width: '100%',
-    height: '400px',
+    height: '606px',
   };
 
   const center = {
-    lat: 0, // Initial center latitude
-    lng: 0, // Initial center longitude
+    lat: -6.257390, // Initial center latitude
+    lng: 106.618397, // Initial center longitude
   };
+
+  const location2 = {
+    lat: -6.325732,
+    lng: 106.990896,
+  }
+
+  const location3 = {
+    lat: -6.014647,
+    lng: 106.676844,
+  }
 
   // Add a console log to ensure the API key is loaded
   console.log(process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY);
 
   return (
     <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || 'AIzaSyBQ9TA-apd_CuM-hSGWJ4PNDFQ8amJ2mF0'}>
-      <GoogleMap mapContainerStyle={mapContainerStyle} center={center} zoom={10}>
+      <GoogleMap mapContainerStyle={mapContainerStyle} center={center} zoom={12}>
         <Marker position={center} />
+        { /* Add other components like DirectionsRenderer here if needed */ }
+        <Marker position={location2} />
+        { /* Add other components like DirectionsRenderer here if needed */ }
+        <Marker position={location3} />
         { /* Add other components like DirectionsRenderer here if needed */ }
       </GoogleMap>
     </LoadScript>
