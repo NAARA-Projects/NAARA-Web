af01f1961ed4f7a3abef51a59d07c1b2df184d0d
Electricman <81240220+ElectricmanYT@users.noreply.github.com>
2024-07-11 21:06:45 +0700

    map page

--- naara/src/app/page.tsx
+++ naara/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex w-full h-full min-h-screen flex-col items-center justify-between p-24 bg-[#0F0F0F]">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <h1 className='text-white'>
-          <Link href='/RequestPage'>map</Link>
+          <Link href='/map'>map</Link>
         </h1>
       </div>
     </main>
