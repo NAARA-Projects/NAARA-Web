3f8593af13133f74685bd88de5f8833b6ad072f2
Electricman <81240220+ElectricmanYT@users.noreply.github.com>
2024-07-11 21:29:00 +0700

    Page Update

--- naara/src/app/page.tsx
+++ naara/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Link from 'next/link';
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex w-full h-full min-h-screen flex-col items-center justify-between p-24 bg-[#0F0F0F]">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+    <main className="flex w-full h-full min-h-screen items-center justify-between bg-[#0F0F0F]">
+      <div className="z-10 w-full max-w-5xl items-center font-mono text-sm">
+        <Image src={`/bg-image.png`} width={1920} height={556} className='w-fit h-auto' alt={'ocean'}/>
         <h1 className='text-white'>
           <Link href='/map'>map</Link>
         </h1>
