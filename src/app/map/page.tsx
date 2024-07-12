784fbf729517279edca59c5613b5cfd0c73643f2
JustinJshvn <81240220+JustinJshvn@users.noreply.github.com>
2024-07-13 01:18:15 +0700

    Update page.tsx

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -68,7 +68,7 @@ const Map: React.FC = () => {
       <main className="w-full flex items-center justify-center">
         <div className="absolute w-[90vw] h-[2000px] bg-[#202020E6] top-[75vh] rounded-t-3xl p-1 backdrop-blur-xl">
           <div className="flex-col flex items-center justify-center">
-            <Image src={"/drag.png"} alt={""} width={133} height={6}/>
+            <Image src={"/drag.png"} alt={""} width={133} height={6} className="mt-2"/>
           <div className="flex-col flex items-center justify-center"></div>
             <div className="flex-row flex items-center justify-center">
               <div className="flex-col flex items-start justify-center">
