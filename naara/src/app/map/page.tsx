1783b64d583c4988d0e6c2d950729603327457c3
Winner R. Rasendriya <118193112+HykoU31@users.noreply.github.com>
2024-07-12 09:19:23 +0700

    Info Window Mentoring

--- naara/src/app/map/page.tsx
+++ naara/src/app/map/page.tsx
@@ -54,29 +54,81 @@ const Map: React.FC = () => {
         <Marker position={location4}
         icon= 'red_MarkerB.png' />
       </GoogleMap>
-    </LoadScript>
+    </LoadScript>    
   );
+  let map, popup, Popup;
+
+/** Initializes the map and the custom popup. */
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -33.9, lng: 151.1 },
+    zoom: 10,
+  });
+  /**
+   * A customized popup on the map.
+   */
+  class Popup extends google.maps.OverlayView {
+    position;
+    containerDiv;
+    constructor(position: any, content: any) {
+      super();
+      this.position = position;
+      content.classList.add("popup-bubble");
+
+      // This zero-height div is positioned at the bottom of the bubble.
+      const bubbleAnchor = document.createElement("div");
+
+      bubbleAnchor.classList.add("popup-bubble-anchor");
+      bubbleAnchor.appendChild(content);
+      // This zero-height div is positioned at the bottom of the tip.
+      this.containerDiv = document.createElement("div");
+      this.containerDiv.classList.add("popup-container");
+      this.containerDiv.appendChild(bubbleAnchor);
+      // Optionally stop clicks, etc., from bubbling up to the map.
+      Popup.preventMapHitsAndGesturesFrom(this.containerDiv);
+    }
+    /** Called when the popup is added to the map. */
+    onAdd() {
+      this.getPanes()!.floatPane.appendChild(this.containerDiv);
+    }
+    /** Called when the popup is removed from the map. */
+    onRemove() {
+      if (this.containerDiv.parentElement) {
+        this.containerDiv.parentElement.removeChild(this.containerDiv);
+      }
+    }
+    /** Called each frame when the popup needs to draw itself. */
+    draw() {
+      const divPosition = this.getProjection().fromLatLngToDivPixel(
+        this.position,
+      );
+      // Hide the popup when it is far out of view.
+      const display =
+        Math.abs(divPosition!.x) < 4000 && Math.abs(divPosition!.y) < 4000
+          ? "block"
+          : "none";
+
+      if (display === "block") {
+        this.containerDiv.style.left = divPosition!.x + "px";
+        this.containerDiv.style.top = divPosition!.y + "px";
+      }
+
+      if (this.containerDiv.style.display !== display) {
+        this.containerDiv.style.display = display;
+      }
+    }
+  }
+
+  popup = new Popup(
+    new google.maps.LatLng(-33.866, 151.196),
+    document.getElementById("content"),
+  );
+  popup.setMap(map);
+}
+
+window.initMap = initMap;
+
 };
 
 export default Map;
 
-/// export default function DashboardPage () {
-  const [places, setPlaces] = useState<Place []>([
-    { //Events
-      name: "Event A",
-      date: "13 July 2024",
-    },
-    { //Events
-      name: "Event B",
-      date: "09 June 2024",
-    },
-    { //Trash Found
-      name: "Trash A",
-      status: "Cleared",
-    },
-    { //Trash Found
-      name: "Trash B",
-      status: "Pending",
-    }
-  ])
-}
