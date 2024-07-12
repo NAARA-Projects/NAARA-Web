35fa3749f19cce50ea70678242033752343f02e1
JustinJshvn <81240220+JustinJshvn@users.noreply.github.com>
2024-07-12 10:02:39 +0700

    Navbar finalized

--- naara/src/app/components/Navbar.tsx
+++ naara/src/app/components/Navbar.tsx
@@ -10,9 +10,9 @@ const Navbar = () => {
     };
     return(
         <header className="fixed z-10 w-full">
-            <nav className="fixed bg-[#0F0F0F] w-full">
-                <div className="relative container flex text-white py-[8px] px-16 flex-row items-center align-center justify-center">
-                    <div className="flex my-[6px] basis-1/4 mr-5 ml-10">
+            <nav className="fixed bg-[#0F0F0F] w-full px-10">
+                <div className="container flex text-white py-[8px] px-16 flex-row items-center align-center justify-center">
+                    <div className="flex my-[6px] mr-5">
                         <div className="flex shrink-0">
                             <Link href='/'><Image src={'/naara.svg'} alt={"logo"} height={25} width={25} className="object-contain"/></Link>
                             <Link href='/' className="ml-4 mr-2 pt-[7px]"><Image src={'/naara..svg'} alt={"logo"} height={24} width={66} className="object-contain"/></Link>
@@ -21,10 +21,10 @@ const Navbar = () => {
                         <Link href='/' className="mx-2"><p>Home</p></Link>
                         <Link href='/map' className="mx-2"><p>Map</p></Link>
                     </div>
-                    <div className="flex-initial flex basis-1/2">
-                        <SearchInput customClass='max-w-[600px] flex-initial shrink mx-5' classInput={undefined} />
+                    <div className="flex-initial flex w-full px-40">
+                        <SearchInput customClass='max-w-[600px] flex-initial shrink' classInput='=' />
                     </div>
-                    <div className="flex flex-initial my-[2px] basis-1/4 ml-5">
+                    <div className="flex flex-initial my-[2px] ml-5">
                         <Link href='/login' className="mx-4 my-[2px] shrink-0"><p>Log in</p></Link>
                         <CustomButton title={"Subscribe"} handleClick={subscribeButtonClick} containerStyles="mx-2 bg-gradient-to-r from-[#CA8F36] to-[#D04C00] rounded-lg px-3 py-[2px]" />
                         <Image src={"/profileHolder.svg"} alt={"profile"} width={32} height={32} className="mx-4"/>
