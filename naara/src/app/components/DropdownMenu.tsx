7a1ed42ae5e26747ee4cf1f80f437ef3b1d5ed9a
Winner R. Rasendriya <118193112+HykoU31@users.noreply.github.com>
2024-07-12 21:17:51 +0700

    Coloured Dropdown Menu Final

--- naara/src/app/components/DropdownMenu.tsx
+++ naara/src/app/components/DropdownMenu.tsx
@@ -1,11 +1,9 @@
-// DropdownMenu.tsx
 'use client';
 import React from 'react';
 
 const DropdownMenu: React.FC = () => {
     return (
         <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg">
-            {/* Example menu items */}
             <div className="py-1">
                 <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Settings</a>
                 <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Contact</a>
--- naara/src/app/components/ProfileDropdown.tsx
+++ naara/src/app/components/ProfileDropdown.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './ProfileDropdown.module.css'; // Adjust the path as needed
+import styles from './ProfileDropdown.module.css';
 
 interface ProfileDropdownProps {
     isOpen: boolean;
@@ -15,9 +15,9 @@ const ProfileDropdown: React.FC<ProfileDropdownProps> = ({ isOpen, onClose }) =>
             onMouseLeave={onClose}
         >
             <ul>
-                <li className="py-2 px-4 hover:bg-gradient-to-r from-[#CA8F36] to-[#D04C00] text-white text-center cursor-pointer">My profile</li>
-                <li className="py-2 px-4 hover:bg-gradient-to-r from-[#5ABBDC] to-[#2457B5] text-white text-center cursor-pointer">Settings</li>
-                <li className="py-2 px-4 hover:bg-gray-100 text-[#B60000] text-center cursor-pointer"><b>Log out</b></li>
+                <li className="py-2 px-4 hover:bg-gradient-to-r from-[#5ABBDC] to-[#2457B5] text-white text-center cursor-pointer">My profile</li>
+                <li className="py-2 px-4 hover:bg-gradient-to-r from-[#CA8F36] to-[#D04C00] text-white text-center cursor-pointer">Settings</li>
+                <li className="py-2 px-4 hover:bg-gradient-to-r from-[#ADE572] to-[#6EB79D] text-[#B60000] text-center cursor-pointer"><b>Log out</b></li>
             </ul>
         </div>
     );
