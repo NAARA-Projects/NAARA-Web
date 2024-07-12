a64169571a8b3788cf01bfb83769093789a36b03
Winner R. Rasendriya <118193112+HykoU31@users.noreply.github.com>
2024-07-12 10:28:25 +0700

    infowindow works but needs fixing

--- naara/src/app/map/page.tsx
+++ naara/src/app/map/page.tsx
@@ -1,6 +1,6 @@
 "use client";
-import React from "react";
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import React, { useState, useCallback} from "react";
+import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
 import { time } from "console";
 
 const Map: React.FC = () => {
@@ -34,6 +34,12 @@ const Map: React.FC = () => {
     lng: 115.429313,
   }
 
+  const [selected, setSelected] = useState<{ lat: number; lng: number } | null>(null);
+
+  const onMarkerClick = useCallback((position: { lat: number; lng: number }) => {
+    setSelected(position);
+  }, []);
+
   // Add a console log to ensure the API key is loaded
 
 
@@ -44,17 +50,69 @@ const Map: React.FC = () => {
       <GoogleMap mapContainerStyle={mapContainerStyle} center={center} zoom={10}>
         <Marker 
         position={location1}
-        icon= 'blue_MarkerB.png' />
+        icon= 'blue_MarkerB.png'
+        onClick={() => onMarkerClick(location1)} 
+        />
+        {selected && (
+          <InfoWindow
+            position={selected}
+            onCloseClick={() => setSelected(null)}
+          >
+            <div>
+              <h2><b>Nusa Dua Beach</b></h2>
+              <p>Event Details: 13 July 2024 - 10.00</p>
+            </div>
+          </InfoWindow>
+        )}
         { /* Add other components like DirectionsRenderer here if needed */ }
         <Marker 
         position={location2}
-        icon= 'red_MarkerA.png' />
+        icon= 'red_MarkerA.png' 
+        onClick={() => onMarkerClick(location2)} 
+        />
+        {selected && (
+          <InfoWindow
+            position={location2}
+            onCloseClick={() => setSelected(null)}
+          >
+            <div>
+              <h2><b>Segara Wilis Beach</b></h2>
+              <p>Trash Found! Host your clean-up event here!</p>
+            </div>
+          </InfoWindow>
+        )}
         { /* Add other components like DirectionsRenderer here if needed */ }
         <Marker position={location3}
-        icon= 'blue_MarkerA.png' />
+        icon= 'blue_MarkerA.png' 
+        onClick={() => onMarkerClick(location3)} 
+        />
+        {selected && (
+          <InfoWindow
+            position={location3}
+            onCloseClick={() => setSelected(null)}
+          >
+            <div>
+              <h2><b>Berawa Beach</b></h2>
+              <p>Event Details: 05 August 2024 - 15.00</p>
+            </div>
+          </InfoWindow>
+        )}
         { /* Add other components like DirectionsRenderer here if needed */ }
         <Marker position={location4}
-        icon= 'red_MarkerB.png' />
+        icon= 'red_MarkerB.png' 
+        onClick={() => onMarkerClick(location4)} 
+        />
+        {selected && (
+          <InfoWindow
+            position={location4}
+            onCloseClick={() => setSelected(null)}
+          >
+            <div>
+              <h2><b>Sandy Bay Beach</b></h2>
+              <p>Trash Found! Host your clean-up event here!</p>
+            </div>
+          </InfoWindow>
+        )}
       </GoogleMap>
     </LoadScript>
     
